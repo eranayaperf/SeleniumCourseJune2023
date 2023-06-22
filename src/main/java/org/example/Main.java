ad3512baf85e6df2033c8e80040e593c38f97296
eranayaperf <136846575+eranayaperf@users.noreply.github.com>
2023-06-22 08:58:57 -0600

    Sesion 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,10 @@ public class Main {
         // Objetos
         // Clases
         // Metodos
-        // Herencia
-        // Abstraccion
+        // Herencia          - Es un mecanismo por el cual una clase permite heredar caracteristicas (metodos y atributos) a otra clase
+        // Abstraccion       - Consiste en seleccionar datos de un conjunto mas grande para mostrar solo los detalles relevantes del objeto
         // Polimorfismo
-        // Encapsulamiento
+        // Encapsulamiento   - 
 
 
         // Objeto
@@ -20,19 +20,38 @@ public class Main {
         // Tipo de dato definido por el usuario que se utiliza como borrador para objetos individuales. Contiene atributos y metodos.
 
 
-        Coche coche1 = new Coche();
-        Coche coche2 = new Coche("rojo", "honda", "2020", "hibrido", 4, 5);
+//        Coche coche1 = new Coche();
+//        Coche coche2 = new Coche("rojo", "honda", "2020", "hibrido", 4, 5);
+//
+//        coche1.llantas = coche2.llantas;
+//        coche1.marca = coche2.marca;
+//        coche1.color = "azul";
+//
+//        coche1.acelerar();
+//        coche2.encender();
+//
+//        System.out.println("El coche 1 tiene " + coche1.llantas + " llantas");
+//        System.out.println("El coche 2 tiene " + coche2.llantas + " llantas");
+//
+//        System.out.println("El coche 1 es " + coche1.marca);
+//        System.out.println("El coche 2 es " + coche2.marca);
 
-        coche1.llantas = 4;
-        coche1.marca = "nissan";
+//        CocheDeportivo porsche = new CocheDeportivo();
+//
+//        porsche.encender();
+//        porsche.acelerar();
+//        porsche.setModoTurbo();
+//        porsche.frenar();
+//        porsche.apagar();
 
-        coche1.acelerar();
-        coche2.encender();
 
-        System.out.println("El coche 1 tiene " + coche1.llantas + " llantas");
-        System.out.println("El coche 2 tiene " + coche2.llantas + " llantas");
+//        Empleado neto = new Salario("Ernesto", "Avenida Siempre Viva 123", "12345678", 50000);
+//        System.out.println("El salario de: " + neto.nombre + " es: " + neto.getSalario());
 
-        System.out.println("El coche 1 es " + coche1.marca);
-        System.out.println("El coche 2 es " + coche2.marca);
+        Persona hombre1 = new Persona();
+
+        hombre1.setNombre("Ernesto");
+
+        System.out.println("El nombre de la persona es: " + hombre1.getNombre());
     }
 }
